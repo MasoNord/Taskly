28abcb645cb5ea6bd13a0dc54da20d81efb937b2
Diemid Berolzin <trotf233@gmail.com>
2026-08-07 12:34:52 +0300

    feat: set up alembic

---
+++ src/taskly/infrastructure/postgresql/__init__.py
@@ -0,0 +1,8 @@
+
+from taskly.infrastructure.postgresql.tables.user import user_table
+from taskly.infrastructure.postgresql.tables.session import session_table
+
+__all__ = [
+    "user_table",
+    "session_table"
+]
